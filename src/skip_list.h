7673d1d8dec50c1e6cc0cb75d3d113ff3b3da93a
Chris W <chris07solaligue@gmail.com>
2022-10-08 18:34:24 -0500

    skip-list base model

--- src/skip_list.h
+++ src/skip_list.h
@@ -5,4 +5,29 @@
 #ifndef SKIPLIST_CH_SKIP_LIST_H
 #define SKIPLIST_CH_SKIP_LIST_H
 
+template<class ElemType>
+class SkipList {
+public:
+    struct node {
+        std::vector<node *> forward;
+        ElemType key; //  key
+        node(int lvl, const ElemType &value);
+    };
+
+    explicit SkipList(int max_lvl);
+
+    node header;
+    int current_lvl;
+    int max_lvl;
+};
+
+template<class ElemType>
+SkipList<ElemType>::SkipList(int max_lvl)
+: max_lvl(max_lvl), header(max_lvl, ElemType()), current_lvl(1) {}
+
+template<class ElemType>
+SkipList<ElemType>::node::node(int lvl, const ElemType &value_)
+: key(value_), forward(lvl, nullptr) {}
+
+
 #endif //SKIPLIST_CH_SKIP_LIST_H
